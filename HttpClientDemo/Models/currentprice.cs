bce75a39910509566b48544ee60edef79ee5187b
Archie Fan <archiefan@gmail.com>
2022-08-17 19:37:49 +0100

    Code Cleanup

--- HttpClientDemo/Models/currentprice.cs
+++ HttpClientDemo/Models/currentprice.cs
@@ -1,9 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HttpClientDemo.Models
+﻿namespace HttpClientDemo.Models
 {
     internal class Currentprice
     {
